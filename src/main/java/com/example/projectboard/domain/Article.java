8afbaa487cc2cd135dbd1edd18f9de7202ffb872
hyunsik <gustmlr94@naver.com>
2023-07-18 14:13:05 +0900

    #6 - 스프링 부트 프로젝트 시작
    스프링 부트 프로젝트 초기화
    들어간기술
    
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/example/projectboard/domain/Article.java
@@ -0,0 +1,14 @@
+package com.example.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+    private Long id;
+    private String title; // 제목
+    private String content; // 게시글 본문 내용
+    private String hashtag; // 해시태그
+    private LocalDateTime createAt; // 생성일시
+    private String createBy; // 생성자
+    private LocalDateTime modifieAt; // 수정일시
+    private String modifiedBy; // 수정자
+}
